c0016819d70bbd728fe48169f9d91a4d0dd22f1e
minjii1014 <75343665+minjii1014@users.noreply.github.com>
2021-02-02 17:10:01 +0900

    Update PostController.java
    test

--- COVID19_Team/src/main/java/www/dream/com/board/controller/PostController.java
+++ COVID19_Team/src/main/java/www/dream/com/board/controller/PostController.java
@@ -14,6 +14,7 @@ import www.dream.com.board.service.PostService;
 import www.dream.com.party.model.PartyVO;
 
 //change
+//test
 @Controller
 @RequestMapping("/post/*")
 public class PostController {
