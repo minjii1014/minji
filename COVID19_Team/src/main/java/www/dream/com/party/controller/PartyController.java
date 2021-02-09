05fac6b704a12b51d0017f431e9e93ccece468c6
jh <jh@DESKTOP-NERQOLE>
2021-02-09 14:33:41 +0900

    jaecheol

--- COVID19_Team/src/main/java/www/dream/com/party/controller/PartyController.java
+++ COVID19_Team/src/main/java/www/dream/com/party/controller/PartyController.java
@@ -1,39 +1,34 @@
-//package www.dream.com.party.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-//
-//import www.dream.com.party.model.UserVO;
-//import www.dream.com.party.service.PartyService;
-//
-//@Controller
-//@RequestMapping("/party/*")
-//public class PartyController {
-//	@Autowired
-//	private PartyService partyService;
-//	
-//	@RequestMapping("listParty")
-//	public void list(Model model) {
-//		model.addAttribute("listParty", partyService.selectAllParty());
-//	}
-//	
-//	@GetMapping("map")
-//	public void registerLocation(@RequestParam("id") long id, Model model) {
+package www.dream.com.party.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import www.dream.com.party.model.UserVO;
+import www.dream.com.party.service.PartyService;
+
+@Controller
+@RequestMapping("/party/*")
+public class PartyController {
+	@Autowired
+	private PartyService partyService;
+	
+	@GetMapping("registerLocation")
+	public void registerLocation() {
+//		@RequestParam("id") long id, Model model
 //		model.addAttribute("id", id);
-//	}
-//
-//	@PostMapping("map")
-//	public String registerLocation(UserVO user, RedirectAttributes rttr) {
-//		partyService.registerLocation(user);
-//		rttr.addFlashAttribute("result", user.getUserId());
-//		
-//		return "redirect:/post/map";
-//	}
-//	
-//}
+	}
+
+	@PostMapping("registerLocation")
+	public String registerLocation(UserVO user, RedirectAttributes rttr) {
+		partyService.registerLocation(user);
+		rttr.addFlashAttribute("result", user.getLatitude());
+		rttr.addFlashAttribute("result", user.getLongitude());
+		
+		return "redirect:/party/registerLocation";
+	}
+	
+}
--- COVID19_Team/src/main/java/www/dream/com/party/service/PartyService.java
+++ COVID19_Team/src/main/java/www/dream/com/party/service/PartyService.java
@@ -1,24 +1,25 @@
-//package www.dream.com.party.service;
-//
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import www.dream.com.party.model.PartyVO;
-//import www.dream.com.party.model.UserVO;
-//import www.dream.com.party.model.mapper.PartyMapper;
-//
-//@Service
-//public class PartyService {
-//	@Autowired
-//	private PartyMapper partyMapper;
-//	
-//	public List<PartyVO> selectAllParty(){
-//		return partyMapper.selectAllParty();
-//	}
-//	
-//	public void registerLocation(UserVO User) {
-//		partyMapper.registerLocation(User);
-//	}
-//}
+package www.dream.com.party.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import www.dream.com.party.model.PartyVO;
+import www.dream.com.party.model.UserVO;
+import www.dream.com.party.model.mapper.PartyMapper;
+
+@Service
+public class PartyService {
+	@Autowired
+	private PartyMapper partyMapper;
+	
+	public List<PartyVO> selectAllParty() {
+		return partyMapper.selectAllParty();
+	}
+	
+	public void registerLocation(UserVO User) {
+		partyMapper.registerLocation(User);
+	}
+	
+}
